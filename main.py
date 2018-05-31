ab128b332ab35671e97b0be80fe4b64ab5701579
wzl <git@wzl.cc>
2018-05-31 15:41:18 +0800

    实现了编辑的功能

--- main.py
+++ main.py
@@ -29,8 +29,11 @@ def index():
         return render_template('index.html', data= all_data)
 
 
-@app.route('/submit', methods=['POST', 'DELETE'])
+@app.route('/submit', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 def submit():
+    if request.method == 'GET':
+        return redirect('/')
+
     if request.method == 'POST':
         data = json.loads(request.data.decode('utf-8'))
         try:
@@ -49,6 +52,7 @@ def submit():
         db.session.close()
         print('committed!')
         return ''
+
     if request.method == 'DELETE':
         data = json.loads(request.data.decode('utf-8'))
         name = data['name'].strip()
@@ -61,6 +65,32 @@ def submit():
             pass
         db.session.commit()
         return ''
+
+    if request.method == 'PATCH':
+        data = json.loads(request.data.decode('utf-8'))
+        name = data['name'].strip()
+
+        phone = int(data['phone'])
+        go = data['go']
+        back = data['back']
+        where = data['where']
+        if (go == '' or back == '' or where == ''):
+            raise ValueError
+
+        try:
+            x = record.query.filter_by(name=name).one()
+            x.phone = phone
+            x.go = go
+            x.back = back
+            x.where = where
+            db.session.commit()
+        except:
+            pass
+        finally:
+            db.session.close()
+        print('committed!')
+        return ''
+
 
 @app.route('/ip', methods=['GET'])
 def getip():
